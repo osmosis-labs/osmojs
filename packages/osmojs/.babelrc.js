9469afd3ccda658b3323e636944f422672203198
Dan Lynch <pyramation@gmail.com>
2022-07-21 17:56:35 -0700

    babel

--- packages/osmojs/.babelrc.js
+++ packages/osmojs/.babelrc.js
@@ -8,6 +8,8 @@ module.exports = (api) => {
       '@babel/proposal-object-rest-spread',
       '@babel/proposal-class-properties',
       '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining',
       '@babel/proposal-export-default-from'
     ],
     presets: useESModules ? ['@babel/typescript'] : ['@babel/typescript', '@babel/env']
